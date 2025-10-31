10e185917f367b4eea5d81ebe0f91ce7414be17d
TheBinKingVII <bintoronob@gmail.com>
2025-10-31 11:20:15 +0700

    setup main_screen

---
+++ lib/features/groupbuy/presentation/pages/groupby_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GroupbyPage extends StatelessWidget {
+  const GroupbyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Groupby Page")));
+  }
+}
